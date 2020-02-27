4971377025f431bb88c6e152accfcdc619a6b796
philzhang01 <philzhang01@gmail.com>
2020-02-26 22:02:59 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+  // add your code here
+  L.mapquest.key = "VpjP97EkMP4liFuMwSYACVhxam9eC8AA";
+
+  var map = L.mapquest.map("map", {
+    center: [32.878974, -117.235934],
+    layers: L.mapquest.tileLayer("map"),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.88, -117.236]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,10 +1,17 @@
 var data = {
-  rsvp: ['ixd@ucsd.edu']
+  rsvp: ["ixd@ucsd.edu"]
 };
 
 /*
  * GET tasks page.
  */
-exports.adminView = function(request, response){
-  response.render('rsvp', data);
+exports.adminView = function(request, response) {
+  response.render("rsvp", data);
+};
+
+exports.addRSVP = function(req, res) {
+  var rsvpEmail = req.body.rsvpEmail;
+  console.log(rsvpEmail);
+  data.rsvp.push(rsvpEmail);
+  res.send(rsvpEmail);
 };
